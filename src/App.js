b4e864320b20505543796dc77e00731e0b856924
TommyG879 <togrzesik@gmail.com>
2022-06-11 14:50:16 -0400

    Guess What?Chicken Butt (With Working Button)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { useState } from "react";
 export default function App_() {
 const [closed, setClosed] = useState(false)
-<p> </p>
+
 return (
   <div>
   <button onClick={() => setClosed(!closed)}>
     Guess What?
     </button>    
     <h1><strong>{closed ? "Chickenbutt" : ""}</strong></h1>
+    <p>Ha you fell for it!!</p>
     </div>
 )
 }
